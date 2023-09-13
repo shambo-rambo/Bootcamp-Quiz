1b958d4c83c4f6d27652781d03bdfb92454f6485
shambo-rambo <121490568+shambo-rambo@users.noreply.github.com>
2023-09-13 10:32:15 +1000

    Update to js

--- script.js
+++ script.js
@@ -1,8 +1,41 @@
 document.addEventListener("DOMContentLoaded", function() {
+  const questions = [
+    {
+      question: "What does html stand for?",
+      options: ["Hyper Transfer Markup Language", "Hyperlinking Text Management Language", "Hyperlink and Text Markup Language", "Hyper Text Markup Language"],
+      correctAnswer: "Hyper Text Markup Language"
+    },
+    {
+      question: "What was Git named after, referring to British slang?",
+      options: ["Type of Car", "A silly person", "A small fish", "A quick movement"],
+      correctAnswer: "A silly person"
+    },
+    {
+      question: "What does CSS stand for?",
+      options: ["Computer Style Sheets", "Cascading Style Systems", "Cascading Sheet Styles", "Cascading Style Sheets"],
+      correctAnswer: "Cascading Style Sheets"
+    },
+    {
+      question: "What does API stand for?",
+      options: ["Application Programming Interface", "Application Programming Index", "Application Programming Internet", "Application Programming Integration"],
+      correctAnswer: "Application Programming Interface"
+    },
+    {
+      question: "What does DOM stand for?",
+      options: ["Document Object Model", "Document Object Management", "Document Object Manipulation", "Document Object Model"],
+      correctAnswer: "Document Object Model"  
+    }
+  ];
   const startButton = document.getElementById("btn");
   const container = document.getElementById("container");
   const gameOverDiv = document.getElementById("game-over");
+  const timeEl = document.getElementById("timer");
+  const questionDiv = document.getElementById("question");
+  const answerButtons = document.querySelectorAll(".a-btn");
   
+  let currentQuestionIndex = 0;
+  let secondsLeft = 60;
+
   // Initially hide the quiz container and game over div
   container.style.display = "none";
   gameOverDiv.style.display = "none";
@@ -16,10 +49,51 @@ document.addEventListener("DOMContentLoaded", function() {
     startTimer();
     showQuestion(currentQuestionIndex);
   });
-});
 
-function endGame() {
+  function startTimer() {
+    timer = setInterval(function() {
+        secondsLeft--;
+        timeEl.textContent = secondsLeft;
+        if (secondsLeft === 0) {
+            clearInterval(timer);
+            endGame();
+        }
+    }, 1000);
+  }
+
+  function endGame() {
     alert("Time's up! Game Over!");
     container.style.display = "none";  // Hide the quiz container
     gameOverDiv.style.display = "block";  // Show the game over div
-}
+  }
+
+  function showQuestion(questionIndex) {
+    // Display the question
+    questionDiv.innerHTML = `<p>${questions[questionIndex].question}</p>`;
+
+    // Display the answer options
+    for (let i = 0; i < answerButtons.length; i++) {
+      answerButtons[i].innerText = questions[questionIndex].options[i];
+      answerButtons[i].onclick = function() {
+        checkAnswer(questions[questionIndex].options[i]);
+      };
+    }
+  }
+
+  function checkAnswer(selectedOption) {
+    if (selectedOption === questions[currentQuestionIndex].correctAnswer) {
+      alert("Correct!");
+    } else {
+      alert("Wrong!");
+    }
+
+    // Move to the next question or end the quiz
+    currentQuestionIndex++;
+    if (currentQuestionIndex < questions.length) {
+      showQuestion(currentQuestionIndex);
+    } else {
+      alert("Quiz Completed!");
+      // Optional: reset the quiz or navigate to a results page
+    }
+  }
+});
